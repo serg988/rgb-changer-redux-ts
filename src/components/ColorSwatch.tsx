21473a72750cc20e4ecab4175afcaa0f944f6c2b
serg988 <serg988@gmail.com>
2022-11-29 14:39:35 +0300

    color value dislayed

--- src/components/ColorSwatch.tsx
+++ src/components/ColorSwatch.tsx
@@ -1,8 +1,18 @@
-
 import { useAppDispatch, useAppSelector } from '../app/hooks'
 
 export const ColorSwatch = () => {
-  const { red, green, blue } = useAppSelector(state=>state.rgb)
+  const { red, green, blue } = useAppSelector((state) => state.rgb)
+
+  function componentToHex(c: number) {
+    var hex = c.toString(16)
+    return hex.length == 1 ? '0' + hex : hex
+  }
+
+  function rgbToHex(r: number, g: number, b: number) {
+    return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b)
+  }
+
+  const color = (red+green+blue) < 277 ? 'white' : 'black'
 
   return (
     <div
@@ -10,6 +20,13 @@ export const ColorSwatch = () => {
       style={{
         backgroundColor: `rgb(${red}, ${green}, ${blue})`,
       }}
-    ></div>
+    >
+      <div>
+        <div className='colorValue' style={{color: color}}>
+          rgb({red}, {green}, {blue})
+        </div>
+        <div className='colorValue' style={{color: color}}>{rgbToHex(red, green, blue)}</div>
+      </div>
+    </div>
   )
 }
